1d90a666b5090b5790098e0012332a450a622c2a
Kanres-GH <ktnrgs1817@gmail.com>
2024-07-25 22:19:33 +0500

    added adaptability

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,42 @@
 <template>
   <div class="h-screen w-screen content-center transition-colors duration-500" :class="backgroundClass">
     <div class="second-bg">
-      <div class="weather-content flex flex-col">
-        <h1 class="text-center mt-3 rounded-full antialiased"><i>Weatherify</i></h1>
+      <div class="weather-content flex flex-col justify-self-center">
+        <h1 class="text-center mt-3 rounded-full"><i>Weatherify</i></h1>
         <h2 class="text-center mt-5 mb-3">Enter a city name</h2>
         <div class="weather-form flex justify-center flex-col gap-5">
-          <input type="text" id="inputt" class="w-full px-12 rounded-lg" placeholder="E.g. 'Almaty'" required>
+          <input type="text" id="inputt" class="w-full px-4 py-2 rounded-lg text-lg md:px-12 md:py-4" placeholder="E.g. 'Almaty'" required>
           <button class="btn" @click="getWeather">Get current weather</button>
         </div>
         <transition name="fade">
-          <div v-if="weatherData" class="weather-result mt-3 p-5 bg-gray-500 rounded-lg shadow-lg transform transition-all duration-500 ease-in-out opacity-0 h-1/2 grid grid-cols-2 gap-4" :class="{ 'opacity-100': weatherData }">
-            <div class="flex items-center col-span-2">
-              <img :src="`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`" class="w-24 h-24" alt="Weather Icon"/>
+          <div v-if="weatherData" class="weather-result mt-3 p-3 bg-gray-500 rounded-lg shadow-lg transform transition-all duration-500 ease-in-out opacity-0 h-1/2 grid grid-cols-1 md:grid-cols-2 gap-4" :class="{ 'opacity-100': weatherData }">
+            <div class="flex items-center col-span-1 md:col-span-2">
+              <img :src="`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`" class="w-16 h-16 md:w-24 md:h-24" alt="Weather Icon"/>
               <div class="ml-4">
-                <h3 class="text-2xl font-bold antialiased">{{ weatherData.name }}</h3>
-                <p class="text-xl mt-2">{{ weatherData.main.temp }}°C</p>
-                <p class="text-lg">{{ weatherData.weather[0].description[0].toUpperCase() + weatherData.weather[0].description.substring(1) }}</p>
+                <h3 class="text-lg font-bold md:text-2xl">{{ weatherData.name }}</h3>
+                <p class="text-base md:text-xl mt-1 md:mt-2">{{ weatherData.main.temp }}°C</p>
+                <p class="text-sm md:text-lg">{{ weatherData.weather[0].description[0].toUpperCase() + weatherData.weather[0].description.substring(1) }}</p>
               </div>
             </div>
-            <div class="flex flex-col items-center p-4 bg-gray-200 rounded-lg">
-              <h3 class="text-xl mt-2">Feels Like</h3>
-              <p>{{ weatherData.main.feels_like }}°C</p>
+            <div class="flex flex-col items-center p-2 bg-gray-200 rounded-lg md:p-4">
+              <i class="fas fa-temperature-low text-2xl md:text-4xl"></i>
+              <h3 class="text-base md:text-xl mt-1 md:mt-2">Feels Like</h3>
+              <p class="text-sm md:text-lg">{{ weatherData.main.feels_like }}°C</p>
             </div>
-            <div class="flex flex-col items-center p-4 bg-gray-200 rounded-lg">
-              <h3 class="text-xl mt-2">Humidity</h3>
-              <p>{{ weatherData.main.humidity }}%</p>
+            <div class="flex flex-col items-center p-2 bg-gray-200 rounded-lg md:p-4">
+              <i class="fas fa-tint text-2xl md:text-4xl"></i>
+              <h3 class="text-base md:text-xl mt-1 md:mt-2">Humidity</h3>
+              <p class="text-sm md:text-lg">{{ weatherData.main.humidity }}%</p>
             </div>
-            <div class="flex flex-col items-center p-4 bg-gray-200 rounded-lg">
-              <h3 class="text-xl mt-2">Condition</h3>
-              <p>{{ weatherData.weather[0].main }}</p>
+            <div class="flex flex-col items-center p-2 bg-gray-200 rounded-lg md:p-4">
+              <i class="fas fa-cloud text-2xl md:text-4xl"></i>
+              <h3 class="text-base md:text-xl mt-1 md:mt-2">Condition</h3>
+              <p class="text-sm md:text-lg">{{ weatherData.weather[0].main }}</p>
             </div>
-            <div class="flex flex-col items-center p-4 bg-gray-200 rounded-lg">
-              <h3 class="text-xl mt-2">Wind Speed</h3>
-              <p>{{ weatherData.wind.speed }} m/s</p>
+            <div class="flex flex-col items-center p-2 bg-gray-200 rounded-lg md:p-4">
+              <i class="fas fa-wind text-2xl md:text-4xl"></i>
+              <h3 class="text-base md:text-xl mt-1 md:mt-2">Wind Speed</h3>
+              <p class="text-sm md:text-lg">{{ weatherData.wind.speed }} m/s</p>
             </div>
           </div>
         </transition>
@@ -85,11 +89,11 @@ const getWeather = async () => {
 }
 
 h3 {
-  font-size: 1.8vw;
+  font-size: 2vw;
   font-weight: bold;
 }
 
 p {
-  font-size: 1.5vw;
+  font-size: 1.6vw;
 }
 </style>
